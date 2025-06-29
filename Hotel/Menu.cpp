f9e2bdfff42e47c54c107c9578d29729b4d7e286
Russbell1-1 <zvalkiria@outlook.es>
2025-06-29 11:35:30 -0500

    Subir funcion menu de Hotel version 1

---
+++ Hotel/Menu.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include "Menu.h"
+using namespace std;
+
+void Menuinicial(){
+	int rpta;
+	do{
+		cout<<" --- Bienvenido al Hotel ---\n";
+		cout<<"1. Trabajor\n";
+		cout<<"2. Cliente\n";
+		cout<<"3. Salir\n";
+		cout<<"Seleccione como desea ingresar al sistema:";
+		cin>>rpta;
+	} while(rpta!=3);
+}
