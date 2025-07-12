7ea5d8dac82f4fa8773af0a0ed67daec25ed37ab
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-07-11 21:22:42 -0500

    Se agrega en Restaurante.cpp funcion gestionarMesa que cuenta con otras 6 funciones

--- Restaurante/Restaurante.cpp
+++ Restaurante/Restaurante.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
-#using namepsace std;
+#include "Restaurante.h"
+using namespace std;
+
+void gestionarMesa(){
+    int opcion;
+    do {
+        cout << "==== GESTION DE MESAS ====";
+        cout << "1. Crear mesas " << endl;
+        cout << "2. Modificar mesas" << endl;    
+        cout << "3. Asignar cliente a mesas" << endl;
+        cout << "4. Unir o dividir mesas " << endl;
+        cout << "5. Eliminar mesas" << endl;
+        cout << "6. Ver mapa de mesas" << endl;
+        cout << "7. Salir" << endl;
+        
+        cout << "Ingrese la opcion" << endl;
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:  
+                break;
+            case 2:  
+                break;
+            case 3:
+                break;
+            case 4: 
+                break;
+            case 5: 
+                break;
+            case 6: 
+                break;
+            case 7: 
+                cout << "Volviendo al menú principal...\n"; break;
+            default: 
+                cout << "Opción inválida.\n";
+        }
+    } while (opcion != 7);
+}
 
 int main(){
-	
+    gestionarMesa();
+	return 0;
 }
