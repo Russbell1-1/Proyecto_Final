cf3bd32d953d654552ebe25fbbc0e5b8cc58437b
Russbell1-1 <zvalkiria@outlook.es>
2025-07-13 13:00:18 -0500

    agrega cpp para mostrar bienvenida final

---
+++ MostrarBienvenidaNoASCII.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<windows.h>
+#include "Menu.h"
+
+using namespace std;
+
+void MostrarBienvenida(){
+	
+	// Usar la funcion para cambiar de color la fuente
+	HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole,14); 
+	system("cls");
+	cout<<"+----------------------------------------------+\n";
+	cout<<"¦                                              ¦\n";
+	cout<<"¦         BIENVENIDO AL SISTEMA DEL HOTEL      ¦\n";
+	cout<<"¦                                              ¦\n";
+	cout<<"¦----------------------------------------------¦\n";
+	cout<<"¦    Gestión de habitaciones y servicios       ¦\n";
+	cout<<"¦    Atención 24/7 - ¡Su comodidad primero!    ¦\n";
+	cout<<"+----------------------------------------------+\n";
+	
+	SetConsoleTextAttribute(hConsole,15); // Restaurar color
+	cout<<"\nPresione una tecla para continuar...";
+	system("pause>nul");
+}
--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 
 int main(){
-	MostrarBienvenida;
+	MostrarBienvenida();
 	return 0;
 }
