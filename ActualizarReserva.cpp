fe8d46c193fc3e725fee3c9b278eb852473aa4d5
ShirleyCauna <caunashirley11@gmail.com>
2025-07-13 22:40:32 -0500

    Comenté función para actualizar reserva de cliente

--- ActualizarReserva.cpp
+++ ActualizarReserva.cpp
@@ -3,16 +3,19 @@
 #include "Menu.h"
 using namespace std;
 
+// Variables externas que se usan para acceder a los datos del cliente y habitaciones
 extern Cliente clientes[MAX_CLIENTES];
 extern int totalClientes;
 extern bool habitaciones[FILAS][COLUMNAS];
 
+// Función que permite actualizar los datos de una reserva ya registrada
 void ActualizarReserva(){
 	if(totalClientes==0){
 		cout<<"No hay clientes registrados.\n";
 		return;
 	}
 
+	// Se solicitan los datos del cliente a buscar
 	char nombreBuscado[30], apellidoBuscado[30];
 	cin.ignore();
 	cout<<"Ingrese el nombre del cliente a actualizar: ";
@@ -20,22 +23,26 @@ void ActualizarReserva(){
 	cout<<"Ingrese el apellido: ";
 	cin.getline(apellidoBuscado,30);
 
+	// Se busca al cliente en el arreglo
 	int indice=-1;
 	for(int i=0;i<totalClientes;i++){
 		bool coincideNombre=true;
 		bool coincideApellido=true;
+		// Comparación de nombre
 		for(int j=0;nombreBuscado[j]!='\0'||clientes[i].nombre[j]!='\0';j++){
 			if(nombreBuscado[j]!=clientes[i].nombre[j]){
 				coincideNombre=false;
 				break;
 			}
 		}
+		// Comparación de apellido
 		for(int j=0;apellidoBuscado[j]!='\0'||clientes[i].apellido[j]!='\0';j++){
 			if(apellidoBuscado[j]!=clientes[i].apellido[j]){
 				coincideApellido=false;
 				break;
 			}
 		}
+		// Si el nombre y apellido coinciden, guardamos el índice del cliente
 		if(coincideNombre&&coincideApellido){
 			indice=i;
 			break;
@@ -47,6 +54,7 @@ void ActualizarReserva(){
 		return;
 	}
 
+	// Mostrar datos actuales del cliente
 	cout<<"Cliente actual:\n";
 	cout<<"Nombre: "<<clientes[indice].nombre<<"\n";
 	cout<<"Apellido: "<<clientes[indice].apellido<<"\n";
@@ -68,11 +76,13 @@ void ActualizarReserva(){
 	MostrarHabitacionesDisponibles();
 	int piso,habi;
 	do{
+		// Selección de nueva habitación
 		cout<<"Seleccione nuevo piso (1-"<<FILAS<<"): ";
 		cin>>piso;
 		cout<<"Seleccione nueva habitacion (1-"<<COLUMNAS<<"): ";
 		cin>>habi;
 
+		// Validación de habitación
 		if(piso<1||piso>FILAS||habi<1||habi>COLUMNAS){
 			cout<<"Piso o habitación fuera de rango.\n";
 		}else if(habitaciones[piso-1][habi-1]){
@@ -82,6 +92,7 @@ void ActualizarReserva(){
 		}
 	}while(true);
 
+	// Se actualizan los datos del cliente
 	clientes[indice].piso=piso;
 	clientes[indice].habitacion=habi;
 	habitaciones[piso-1][habi-1]=true;
